e298e0be584051cacbd74edb79a9f9962dbf7f83
Gabriel Kaustchr <gabrielkaustchr@gmail.com>
2022-07-26 20:29:38 -0300

    Aula 09 criando FORCA

---
+++ aulas/aula09/aula09.py
@@ -0,0 +1,2 @@
+print("Hello, World")
+print('Ola. mundo!')
---
+++ aulas/aula09/forca.py
@@ -0,0 +1,57 @@
+# JOGO DA FORCA 
+from random import randint
+
+# Criei uma lista de palavras pro jogo 
+# LISTA OU VETOR 
+palavras = ['Casa', 'Carro', 'Moto', 'Mesa', 'Cadeira', 'Teto', 'Casarao', 'Terreno', 'Escola', 'Curso', 'Musica', 'Montanha', 'Piscina', 'Pia', 'Porto', 'Pa', 'Padaria', 'Empresa', 'Ilha', 'Televisao', 'Radio', 'Rua', 'Planta', 'Arvore', 'Portao', 'Computador', 'Escada', 'Sofa']
+
+indice = randint(0, len(palavras) - 1) # Recebe um numero aleatorio entre 0 e a quantidade total de palavras da lista
+palavra_secreta =  palavras[indice] # Palavra do jogo 
+tentativa = [] # LISTA DA PALAVRA  _ _ _ _ _  -> P _ _ T _
+chutes = [] # LISTA DE CHUTES ERRADOS -> W, S, M, J, P
+
+for i in range(len(palavra_secreta)):
+    tentativa.append('_')
+
+print(palavra_secreta)
+print(tentativa)
+
+def exibirMsg(msg):
+    print(msg)
+
+def encontraLetra(chute): # O
+    temLetra = False
+    for i, letra in enumerate(palavra_secreta): # M O T O
+        if(chute.upper() == letra.upper()):
+            tentativa[i] = chute.upper()
+            temLetra = True
+    return temLetra
+
+def jogar():
+    tentativas = 5
+    while(True):
+        chute = input('Digite uma letra R: ') # O
+        if(encontraLetra(chute)):
+            exibirMsg(tentativa)
+        else:
+            exibirMsg(f'Letra {chute} não encontrada')
+            chutes.append(chute.upper())
+            exibirMsg(chutes)
+            tentativas -= 1
+            exibirMsg(f'Restam {tentativas} tentativas')
+            exibirMsg(tentativa)
+        
+        if(tentativas <= 0):
+            exibirMsg('VOCÊ PERDEU ;( \nJOGUE NOVAMENTE!')
+            exibirMsg(f'A palavra secreta era {palavra_secreta.upper()}')
+            break
+
+
+while(True): # continuar == True
+    exibirMsg('**** JOGO DA FORCA ****')
+    menu = int(input(' 1 - Jogar \n 2 - Sair \n R:'))
+    if(menu == 1):
+        jogar()
+    else:
+        exibirMsg('Tchau!!!!')
+        break
